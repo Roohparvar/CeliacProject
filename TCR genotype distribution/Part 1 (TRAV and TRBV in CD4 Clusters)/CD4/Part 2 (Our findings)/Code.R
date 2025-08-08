97ae0d6af019dc4de9cd2cc655598567fc4a4681
Esmaeil Roohparvar Basmenj <e.roohparvar@modares.ac.ir>
2025-08-08 18:03:14 +1000

    Distribution TRAV and TRBV in CD4

--- TCR genotype distribution/Part 1 (TRAV and TRBV in CD4 Clusters)/CD4/Part 2 (Our findings)/Code.R
+++ TCR genotype distribution/Part 1 (TRAV and TRBV in CD4 Clusters)/CD4/Part 2 (Our findings)/Code.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(scales)
 
 # Desired cluster order
-cluster_order <- c("Th2/Tfh", "Tregs")
+cluster_order <- c("Th1 Mem", "Th17", "Th2/Tfh", "Tregs")
 full_metadata$cluster <- factor(full_metadata$cluster, levels = cluster_order)
 full_metadata <- full_metadata %>% filter(!is.na(cluster))
 
@@ -19,12 +19,12 @@ total_counts <- filtered_data %>%
 
 selected_data <- filtered_data %>%
   mutate(tcr_group = case_when(
-    TRAV == "TRAV26-1" & TRBV == "TRBV7-2" ~ "TRAV26-1 + TRBV7-2",
-    TRAV == "TRAV4" & TRBV == "TRBV4" ~ "TRAV4 + TRBV4",
-    TRAV == "TRAV4" & TRBV == "TRBV7-2" ~ "TRAV4 + TRBV7-2",
-    TRBV == "TRBV20-1" ~ "TRBV20-1",
-    TRBV == "TRBV29-1" ~ "TRBV29-1",
-    TRBV == "TRBV9" ~ "TRBV9",
+    TRAV == "TRAV13-1" & TRBV == "TRBV20-1" ~ "TRAV13-1 + TRBV20-1",
+    TRAV == "TRAV9-2" & TRBV == "TRBV20-1" ~ "TRAV9-2 + TRBV20-1",
+    TRAV == "TRAV12-2" & TRBV == "TRBV20-1" ~ "TRAV12-2 + TRBV20-1",
+    TRAV == "TRAV29/DV5" & TRBV == "TRBV5-1" ~ "TRAV29/DV5 + TRBV5-1",
+    TRAV == "TRAV13-1" & TRBV == "TRBV5-1" ~ "TRAV13-1 + TRBV5-1",
+    TRAV == "TRAV13-1" & TRBV == "TRBV7-2" ~ "TRAV13-1 + TRBV7-2",
     TRUE ~ NA_character_
   )) %>%
   filter(!is.na(tcr_group))
@@ -41,12 +41,12 @@ bar_data <- group_counts %>%
 
 
 tcr_colors <- c(
-  "TRAV26-1 + TRBV7-2" = "#ae1e28",
-  "TRAV4 + TRBV4"      = "#f78454",
-  "TRAV4 + TRBV7-2"    = "#ffd8c5",
-  "TRBV20-1"           = "#cfe3f6",
-  "TRBV29-1"           = "#5eaada",
-  "TRBV9"              = "#1367b7"
+  "TRAV13-1 + TRBV20-1" = "#ae1e28",
+  "TRAV9-2 + TRBV20-1"      = "#f78454",
+  "TRAV12-2 + TRBV20-1"    = "#ffd8c5",
+  "TRAV29/DV5 + TRBV5-1"           = "#cfe3f6",
+  "TRAV13-1 + TRBV5-1"           = "#5eaada",
+  "TRAV13-1 + TRBV7-2"              = "#1367b7"
 )
 
 
@@ -55,7 +55,7 @@ bar_plot <- ggplot(bar_data, aes(x = cluster, y = freq, fill = tcr_group)) +
   scale_fill_manual(values = tcr_colors, name = "TCR Group") +
   scale_y_continuous(labels = label_percent(scale = 1)) +
   labs(x = "Cluster", y = "Percentage of Cells",
-       title = "Proportion of Specific TCR Gene Combinations Across Clusters") +
+       title = "Proportion of Specific TCR Gene Combinations Across CD4 Clusters") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 

f9830891e12db02a7def7add1d3c22787dd4e08c
Esmaeil Roohparvar Basmenj <e.roohparvar@modares.ac.ir>
2025-09-23 08:35:00 +1000

    Found shared clones and implemented exact shared clone metadata extraction

---
+++ shared gd/in 5 Tgd Clusters/code.R
@@ -0,0 +1,67 @@
+library(dplyr)
+library(tidyr)
+library(UpSetR)
+library(tibble)
+
+
+full_metadata = full_metadata[full_metadata$cluster == "Tgd" |
+                                full_metadata$cluster == "Tgd CD8+" |
+                                full_metadata$cluster == "NK Tgd" |
+                                full_metadata$cluster == "Tgd INSIG1+" |
+                                full_metadata$cluster == "Trm IEL", ]
+
+full_metadata <- full_metadata %>% filter( !is.na(full_metadata$TRGV) & !is.na(full_metadata$TRGV) )
+
+
+
+# Create a named list of clones per cluster
+clones_by_cluster <- full_metadata %>%
+  filter(!is.na(cdr_Full_gd)) %>%      # remove NAs
+  group_by(cluster) %>%
+  summarise(clones = list(unique(cdr_Full_gd)))
+
+clones_list <- setNames(clones_by_cluster$clones, clones_by_cluster$cluster)
+
+# Convert to UpSet input
+upset_data <- fromList(clones_list)
+
+# Save as PNG
+png("shared_clones_Tgd_Clusters.png", width = 2000, height = 2300, res = 300, bg = "white")
+
+upset(upset_data,
+      nsets = length(clones_list),
+      nintersects = 20,
+      order.by = "freq",
+      mainbar.y.label = "Shared Clones",
+      sets.x.label = "Clones per Tγδ Clusters")
+
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+get_exact_shared_metadata <- function(metadata, clones_list, clusters) {
+  # کلون‌های منحصراً مشترک
+  common <- Reduce(intersect, clones_list[clusters])
+  
+  exact <- common[sapply(common, function(cl) {
+    which_sets <- names(which(sapply(clones_list, function(clst) cl %in% clst)))
+    setequal(sort(which_sets), sort(clusters))
+  })]
+  
+  # فیلتر متادیتا بر اساس همین کلون‌ها
+  result_meta <- metadata %>% filter(cdr_Full_gd %in% exact)
+  
+  return(result_meta)
+}
+
+
+new_meta <- get_exact_shared_metadata(full_metadata, clones_list, 
+                                      c("Trm IEL", "Tgd INSIG1+"))

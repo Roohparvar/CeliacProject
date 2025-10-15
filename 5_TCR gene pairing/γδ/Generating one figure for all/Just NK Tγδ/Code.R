bf5755bce4c8371a684cd267dcb05baebd136c7b
Esmaeil Roohparvar Basmenj <e.roohparvar@modares.ac.ir>
2025-10-15 19:02:30 +1100

    shared clones

---
+++ 5_TCR gene pairing/γδ/Generating one figure for all/Just NK Tγδ/Code.R
@@ -0,0 +1,97 @@
+setwd("C:/Esmaeil/CeliacProject/CeliacProject/5_TCR gene pairing/γδ/Generating one figure for all/Just NK Tγδ")
+
+library(dplyr)
+library(circlize)
+library(ggplot2)
+library(ggalluvial)
+library(scales)
+library(tidyr)
+library(writexl)
+
+
+full_metadata$cluster <- recode(full_metadata$cluster,
+                                "Tgd INSIG1+" = "Tγδ INSIG1+",
+                                "NK/Tgd" = "NK/Tγδ",
+                                "Act. Tgd" = "Act. Tγδ",
+                                "Tgd CD8+" = "Tγδ CD8+"
+)
+
+
+full_metadata = full_metadata[full_metadata$cluster == "NK/Tγδ" , ]
+
+
+full_metadata <- full_metadata %>% filter( !is.na(full_metadata$TRDV) & !is.na(full_metadata$TRGV) )
+
+
+RCD22 = full_metadata[full_metadata$Patient == "RCD2-2",]
+RCD21b = full_metadata[full_metadata$Patient == "RCD2-1b",]
+
+table(RCD22$TRDV)
+table(RCD21b$TRDV)
+
+
+result <- full_metadata %>%
+  group_by(Diagnosis, TRDV) %>%
+  summarise(TRGVs = paste(unique(TRGV), collapse = ","), .groups = "drop") %>%
+  arrange(Diagnosis, TRDV)
+
+
+
+
+
+all_sectors <- unique(c(full_metadata$TRDV, full_metadata$TRGV))
+all_sectors <- na.omit(all_sectors)
+
+# Example: define your own colors
+custom_colors <- c(
+  "TRDV1"  = "#c6ab52",   
+  "TRDV2"  = "#679966",   
+  "TRDV3"  = "#ff6766",    
+  "TRGV2"  = "#ae1f29",
+  "TRGV3"  = "#e05b48",
+  "TRGV4"  = "#f1a284",
+  "TRGV5"  = "#fbdbc3",
+  "TRGV5P" = "#f7f7f7",
+  "TRGV8"  = "#cce5f6",
+  "TRGV9"  = "#8fc3dd",
+  "TRGV10" = "#4790be"
+)
+
+# Assign any sectors not listed above to grey
+grid.col <- setNames(rep("grey", length(all_sectors)), all_sectors)
+grid.col[names(custom_colors)] <- custom_colors
+
+
+TRDV_order <- sort(unique(na.omit(full_metadata$TRDV)))
+
+other_sectors <- setdiff(unique(c(full_metadata$TRDV, full_metadata$TRGV)), TRDV_order)
+sector_order <- c(TRDV_order, other_sectors)
+
+
+
+
+# chordDiagram
+Diagnosiss <- unique(full_metadata$Diagnosis)
+
+for (p in Diagnosiss) {
+  
+  df <- subset(full_metadata, Diagnosis == p, select = c("TRDV", "TRGV"))
+  df <- na.omit(df)
+  
+  if (nrow(df) > 0) {
+
+    
+    ## --- PNG output ---
+    png(file = paste0("Circos_", p, ".png"), width = 2300, height = 2280, res = 300)
+    chordDiagram(df, 
+                 order = sector_order,
+                 transparency = 0.5,
+                 annotationTrack = "grid",
+                 preAllocateTracks = list(track.height = 0.01), grid.col = grid.col,)
+    
+    dev.off()
+    circos.clear()
+  }
+}
+
+
